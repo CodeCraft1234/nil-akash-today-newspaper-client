fee7d72c12c00ea18803aff0e32f54a9e8349235
AsadFahimTEC <fah485434@gmail.com>
2024-07-15 22:06:08 -0700

    responsive issue solve properly

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
       <Headline></Headline>
       <SubHome></SubHome>
 
-      <TwoCard></TwoCard>
+      {/* <TwoCard></TwoCard> */}
 
       {/* <TwoCard></TwoCard> */}
       <Sikkha></Sikkha>
